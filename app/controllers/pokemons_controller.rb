42c2866975a1b8fc06f4fd204895cf88bf2dee5d
vrunda2712 <angy.v27@gmail.com>
2017-06-29 11:52:29 -0400

    Added params to search GIF

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -4,7 +4,7 @@ class PokemonsController < ApplicationController
       res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/", followlocation: true)
       body = JSON.parse(res.body)
 
-      res_gif = Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIPHY_KEY']}&q=pikachu&rating=g", followlocation: true)
+      res_gif = Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIPHY_KEY']}&q=#{body["name"]}", followlocation: true)
       body_gif = JSON.parse(res_gif.body)
 
       render json: {
