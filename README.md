7d36486a49a9205285ec10ae54c213b2f35b2927
vrunda2712 <angy.v27@gmail.com>
2017-06-29 11:50:29 -0400

    Adding GIFs response

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # PokeGIFs
 
-[See the assignment in Alexa](https://alexa.bitmaker.co/wdi/may-2017/assignments/2604)
+[See the assignment in Alexa](https://alexa.bitmaker.co/wdi/may-2017/assignments/2604
--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -4,10 +4,14 @@ class PokemonsController < ApplicationController
       res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/", followlocation: true)
       body = JSON.parse(res.body)
 
+      res_gif = Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIPHY_KEY']}&q=pikachu&rating=g", followlocation: true)
+      body_gif = JSON.parse(res_gif.body)
+
       render json: {
-        "id": body["id"],
-        "name": body["name"],
-        "type": body["types"][0]['type']['name']
+        url: body_gif["data"][0]["embed_url"],
+        id: body["id"],
+        name: body["name"],
+        type: body["types"][0]['type']['name']
         # results:[
         #   body["id"],body["name"],body["types"][0]['type']['name']
         # ]
